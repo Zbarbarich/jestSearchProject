b41cf86db7b3e007c85b4942c36138b9a0ae47ae
Zach Barbarich <zachery.barbarich@gmail.com>
2024-10-23 22:41:01 -0400

    committing all jest test files

---
+++ praveenVidOne.test.js
@@ -0,0 +1,20 @@
+// longestCommonPrefix.test.js
+import longestCommonPrefix from './longestCommonPrefix';
+
+describe('Longest Common Prefix Function', () => {
+    test('finds the longesty common prefix in ["flower", "flow", "flight"]', () => {
+        expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe("fl");
+    });
+
+    test('returns an empty string for ["dog", "racecar", "car"]', () => {
+        expect(longestCommonPrefix(["dog", "racecar", "car"])).toBe("");
+    });
+
+    test('finds the longest common prefix in ["interspecies", "interstellar", "interstate"]', () => {
+        expect(longestCommonPrefix(["interspecies", "interstellar", "interstate"])).toBe("inter")
+    });
+
+    test('returns an empty string for an empty array', () => {
+        expect(longestCommonPrefix([])).toBe("");
+    });
+});
